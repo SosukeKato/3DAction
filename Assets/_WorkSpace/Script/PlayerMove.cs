2abc5bfbf3ae9c6bac999cabf1e488cffa18bc34
SosukeKato <dokindokinpiano@gmail.com>
2025-08-06 16:37:00 +0900

    [Update]PlayerMove.csにPlayerの移動処理を追加

--- Assets/_WorkSpace/Script/PlayerMove.cs
+++ Assets/_WorkSpace/Script/PlayerMove.cs
@@ -7,7 +7,12 @@ public class PlayerMove : MonoBehaviour
     Rigidbody _rb;
 
     float x;
+    float y;
     float z;
+    Vector3 _velocity;
+
+    [SerializeField]
+    float _moveSpeed;
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
@@ -16,5 +21,8 @@ public class PlayerMove : MonoBehaviour
     {
         x = Input.GetAxisRaw("Horizontal");
         z = Input.GetAxisRaw("Vertical");
+
+        _velocity = new Vector3(x, y, z);
+        _rb.velocity = _velocity * _moveSpeed;
     }
 }
