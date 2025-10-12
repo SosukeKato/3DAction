7b89550118565f7feff9b7ae5fbfe411260a6a59
SosukeKato <dokindokinpiano@gmail.com>
2025-10-12 20:45:37 +0900

    [update]衝突したオブジェクトの情報を取得

--- Assets/_WorkSpace/Script/AttackObject.cs
+++ Assets/_WorkSpace/Script/AttackObject.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    GameObject _hitObj;
+    private void OnTriggerEnter(Collider trg)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _hitObj = trg.gameObject;
     }
 }
