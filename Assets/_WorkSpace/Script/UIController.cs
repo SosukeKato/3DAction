2b5a2d84764c9f41097dc07e0369e951567fac2d
SosukeKato <dokindokinpiano@gmail.com>
2025-10-30 22:01:27 +0900

    [update]PlayerHealthを取得する処理を追加

--- Assets/_WorkSpace/Script/UIController.cs
+++ Assets/_WorkSpace/Script/UIController.cs
@@ -3,13 +3,15 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    PlayerHealth _ph;
+
     [SerializeField]
     Image _buffGauge;
     [SerializeField]
     Image _lifeBar;
     void Start()
     {
-        
+        _ph = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
     }
 
     void Update()
