e67433f1463f3e991963e591f45993a729bf2284
SosukeKato <dokindokinpiano@gmail.com>
2025-10-30 21:53:58 +0900

    [update]UI反映をしなければならないimageを指定できるように変更

--- Assets/_WorkSpace/Script/UIController.cs
+++ Assets/_WorkSpace/Script/UIController.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    [SerializeField]
+    Image _buffGauge;
+    [SerializeField]
+    Image _lifeBar;
     void Start()
     {
         
